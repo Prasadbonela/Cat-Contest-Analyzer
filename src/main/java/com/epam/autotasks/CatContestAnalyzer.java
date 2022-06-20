3f19cfacd4883e538cd0dc27365fb820c122cfa0
Sofia Krylova <sofia_krylova@epam.com>
2022-06-20 14:45:14 +0200

    Initial

---
+++ src/main/java/com/epam/autotasks/CatContestAnalyzer.java
@@ -0,0 +1,42 @@
+package com.epam.autotasks;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+
+public class CatContestAnalyzer {
+
+    public static final Integer DEFAULT_VALUE = -1;
+
+    public Integer getMaxResult(List<Cat> cats) {
+        return null;
+    }
+
+    public Integer getMinResult(List<Cat> cats) {
+        return null;
+    }
+
+    public OptionalDouble getAverageResultByBreed(List<Cat> cats, Cat.Breed breed) {
+        return null;
+    }
+
+    public Optional<Cat> getWinner(List<Cat> cats) {
+        return null;
+    }
+
+    public List<Cat> getThreeLeaders(List<Cat> cats) {
+        return null;
+    }
+
+    public boolean validateResultSumNotNull(List<Cat> cats) {
+        return false;
+    }
+
+    public boolean validateAllResultsSet(List<Cat> cats) {
+        return false;
+    }
+
+    public Optional<Cat> findAnyWithAboveAverageResultByBreed(List<Cat> cats, Cat.Breed breed) {
+        return null;
+    }
+}
